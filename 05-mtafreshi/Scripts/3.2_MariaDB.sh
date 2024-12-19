b297e887c5355d51211e8f55d22c2042cb239a1d
sudoer-x <mohsen.m.tafreshi@gmail.com>
2024-12-19 05:25:11 +0330

    LVM_Function

--- 05-mtafreshi/Scripts/3.2_MariaDB.sh
+++ 05-mtafreshi/Scripts/3.2_MariaDB.sh
@@ -16,17 +16,13 @@ LVM ()
         done
     sudo lsblk
     sleep 5
-    for (( i=1; i<=${num}; i++ ));
-        do
-            pvcreate "${dev}"`cat file"${i}"`
-        done
-    pvdisplay
-    sleep 5
     paste -s -d " " file > row
+    pvcreate `cat row`
     vgcreate myvg `cat row`
     vgdisplay myvg | grep -i "vg size" | awk '{print $3}' > size
     sed -i 's/<//g' size
     vgdisplay myvg | grep -i "vg size" | awk '{print $4}' > size1
     sed -i 's/iB//g' size1
-    lvcreate --name mylv --size ${size}${size1} myvg    
+    lvcreate --name mylv --size ${size}${size1} myvg
+    rm size size1 row
 }
