598ede07b8e97eaedae42c761e19bc28e6cbbed2
sudoer-x <mohsen.m.tafreshi@gmail.com>
2024-12-29 11:35:18 +0330

    CH_Nginx

--- 05-mtafreshi/Scripts/3.3_Nginx_R_Proxy.sh
+++ 05-mtafreshi/Scripts/3.3_Nginx_R_Proxy.sh
@@ -16,23 +16,25 @@ nginx_reverse ()
     sudo apt install nginx iptables-persistent -y
     systemctl start nginx
     systemctl enable nginx
-    systemctl status nginx
-    send q
+    sleep 5
 }
 
 config_proxy ()
 {
     path=/etc/nginx/sites-available/reverse-proxy
     file=./host_config
-    tail -n 2 ${file} | head -n 1 ${file} | awk '{print $1}' > ng
+    tail -n 2 ${file} > ngtemp 
+    head -n 1 ngtemp | awk '{print $1}' > ng
     head -n 1 ${file} | awk '{print $1}' > web
     unlink /etc/nginx/sites-enabled/default
     echo "server {" > ${path}
     echo "    listen 80;" >> ${path}
-    echo "    server_name `cat ng`;" >> ${path}
+    echo "    server_name _;" >> ${path}
+    echo "    access_log /var/log/reverse-access.log;" >> ${path}
+    echo "    error_log /var/log/reverse-error.log;" >> ${path}
     echo "" >> ${path}
     echo "    location / {" >> ${path}
-    echo "        proxy_pass http://`cat web`:80;" >> ${path}
+    echo "        proxy_pass http://`cat web`/;" >> ${path}
     echo "    }" >> ${path}
     echo "}" >> ${path}
     cat ${path}
@@ -41,6 +43,25 @@ config_proxy ()
     nginx -t
     systemctl restart nginx
     echo "Configuration Completed. "
+}
+
+iptable ()
+{
+    iptables -A INPUT -d `cat ng` -p TCP --dport 22 -j ACCEPT
+    iptables -A INPUT -d `cat ng` -p TCP --sport 22 -j ACCEPT
+    iptables -A INPUT -s `cat web` -d `cat ng` -p TCP --sport 80 -j ACCEPT
+    iptables -A INPUT -d `cat ng` -p TCP --dport 80 -j ACCEPT
+    iptables -A OUTPUT -s `cat ng` -p TCP --sport 22 -j ACCEPT
+    iptables -A OUTPUT -s `cat ng` -p TCP --dport 22 -j ACCEPT
+    iptables -A OUTPUT -s `cat ng` -d `cat web` -p TCP --dport 80 -j ACCEPT
+    iptables -A OUTPUT -s `cat ng` -p TCP --sport 80 -j ACCEPT
+    iptables -nvL
+    sleep 5
+    iptables -P INPUT DROP
+    iptables -P OUTPUT DROP
+    iptables -nvL
+    sleep 5
+    iptables-save > /etc/iptables/rules.v4
 }
 
 check_nginx ()
@@ -64,5 +85,6 @@ main ()
 {
     check_root
     check_nginx
+    iptable
 }
 main
